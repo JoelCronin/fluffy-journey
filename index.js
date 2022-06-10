f0ae975b8711880c5c30525b77cee568a4c0d522
joel cronin <you@example.com>
2022-06-10 16:41:57 -0400

    Inquirer questions now cycling through

--- index.js
+++ index.js
@@ -1,81 +1,6 @@
-//Questions asked to User
-const managerQuestions = [
-    {
-        name: "managerName",
-        message: "What is the name of the manager of this team?",
-        type: "input"
-      },
-      {
-        name: "managerID",
-        message: "What is the manager's ID number?",
-        type: "input" 
-      },
-      {
-        name: "managerEmail",
-        message: "What is the managers email?",
-        type: "input" 
-      },
-      {
-        name: "managerOffice",
-        message: "What is the managers office number?",
-        type: "input" 
-      },  
-];
-
-const employeeTypeQuestion = [
-    {
-        name: "nextEmployee",
-        message: "Would you like to add any further employees?",
-        type: "list",
-        choices: ["Engineer", "Intern", "No, I am finished"]
-        },
-]
-
-const engineerQuestions = [
-    {
-        name: "engName",
-        message: "What is the name of this engineer?",
-        type: "input"
-      },
-      {
-        name: "engID",
-        message: "What is this engineer's ID number?",
-        type: "input" 
-      },
-      {
-        name: "engEmail",
-        message: "What is this engineer's email?",
-        type: "input" 
-      },
-      {
-        name: "engGitHub",
-        message: "What is this engineer's GitHub profile tag?",
-        type: "input" 
-      },  
-];
-
-const internQuestions = [
-    {
-        name: "internName",
-        message: "What is the name of this intern?",
-        type: "input"
-      },
-      {
-        name: "internID",
-        message: "What is this intern's ID number?",
-        type: "input" 
-      },
-      {
-        name: "internEmail",
-        message: "What is this intern's email?",
-        type: "input" 
-      },
-      {
-        name: "InternGitHub",
-        message: "What is this Intern's GitHub profile tag?",
-        type: "input" 
-      },  
-];
+const fileQuestions = require("./questions")
+const inquirer = require("inquirer");
+const fs = require('fs');
 
 
 function Employee(name, id, email){
@@ -95,4 +20,50 @@ function Employee(name, id, email){
         console.log(`I am an Employee`)
     }
 }
+
+function askManagerQuestions(){
+  inquirer.prompt(fileQuestions.managerQuestions)
+  .then((answers) => {
+    console.log(answers);
+    askEmployeeTypeQuestion();
+  })
+}
+
+
+function askEmployeeTypeQuestion(){
+  inquirer.prompt(fileQuestions.employeeTypeQuestion)
+  .then((answer) => {
+    console.log(answer);
+    if(answer.nextEmployee === "Engineer"){
+      askEngineerQuestions();
+    } else if (answer.nextEmployee === "Intern"){
+      askInternQuestions();
+    } else {
+      renderHTML();
+    }
+  })
+}
+
+function askEngineerQuestions(){
+  inquirer.prompt(fileQuestions.engineerQuestions)
+  .then((answers) => {
+    console.log(answers);
+    askEmployeeTypeQuestion();
+  })
+}
+
+function askInternQuestions(){
+  inquirer.prompt(fileQuestions.internQuestions)
+  .then((answers) => {
+    console.log(answers);
+    askEmployeeTypeQuestion();
+  })
+}
+
+function renderHTML (){
+  console.log("finished")
+}
+
+
+askManagerQuestions()
 
